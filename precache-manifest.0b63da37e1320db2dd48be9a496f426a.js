d516877b0f3a5d9a72798501396d34afa8721502
jainer <jainermunoz@smash.money>
2020-10-10 11:17:51 -0400

    Updates

---
+++ precache-manifest.0b63da37e1320db2dd48be9a496f426a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebdf6d00f9f9d1b2c8c1145943792fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b80642acfdad84b206c6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e4ef8afb0e482e470f8",
+    "url": "/static/js/2.7b6c5140.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b6c5140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80642acfdad84b206c6",
+    "url": "/static/js/main.4c8bddd8.chunk.js"
+  },
+  {
+    "revision": "f58efaeb893aca0e274c",
+    "url": "/static/js/runtime-main.8e78efc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
